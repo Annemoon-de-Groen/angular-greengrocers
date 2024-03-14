e81bd8d7662433b2bd1c4f832a4691d6349f9114
Annemoon_de_Groen <annemoon2003@gmail.com>
2024-03-14 11:58:59 +0100

    Added filtering and sorting

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,10 +9,10 @@ import { AppComponent } from '../app.component';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent {
-
   constructor(private readonly storeService: StoreService) {
   }
   items = this.storeService.store
+  filteredList: Promise<Item[]> = this.items
 
   getImage(id: string): string {
     return `assets/icons/${id}.svg`
@@ -20,4 +20,21 @@ export class StoreComponent {
   addToCart(item: Item) {
     this.storeService.addItemToCart(item)
   }
+
+
+
+  getFilteredList(filter: string) {
+    if (filter === "") this.filteredList = this.items
+    else {
+      this.filteredList = this.items.then((data) => data.filter((x) => x.type === filter))
+    }
+  }
+
+  sortList(sortMethod: string) {
+    if (sortMethod === 'price') this.filteredList.then((data) => data.sort((a, b) => a.price - b.price))
+    if (sortMethod === 'name') this.filteredList.then((data) => data.sort((a, b) => a.name.localeCompare(b.name)))
+
+  }
+
+
 }
